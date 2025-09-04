d5151e58a72dea2b949d1a1c898ee9d1f5a4c31e
wesley <anastasicrl@gmail.com>
2025-09-04 16:10:52 -0500

    add sqlalchemy to the backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+
+
+engine = create_engine("postgresql+psycopg2://postgres:postgres@db:5432/DB")
+conn = engine.connect()
+
 
 app = FastAPI()
 
