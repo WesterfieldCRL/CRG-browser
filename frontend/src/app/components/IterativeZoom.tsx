6c7556787252218a61d250b35ac190bf62e27910
Wesley Anastasi <anastasicrl@gmail.com>
2025-10-09 19:44:55 -0500

    updated one style to use class

--- frontend/src/app/components/IterativeZoom.tsx
+++ frontend/src/app/components/IterativeZoom.tsx
@@ -138,18 +138,7 @@ export default function Zoom({gene_name, onValueChange}: ZoomProps) {
                     </div>
                     <button 
                         onClick={() => handleButtonPress()}
-                        style={{
-                            padding: '10px 16px',
-                            borderRadius: '6px',
-                            border: '1px solid #123c7c',
-                            backgroundColor: '#123c7c',
-                            color: 'white',
-                            cursor: 'pointer',
-                            transition: 'background-color 0.2s ease, box-shadow 0.2s ease',
-                            minWidth: '90px',
-                            userSelect: 'none',
-                            marginTop: '40px'
-                        }}
+                        className='select'
                         onMouseOver={(e) => {
                             e.currentTarget.style.backgroundColor = '#0d2a55';
                             e.currentTarget.style.boxShadow = '0 0 6px rgba(18, 60, 124, 0.6)';
