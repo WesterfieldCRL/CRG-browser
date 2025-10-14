bf22ed7b06ed257128ec2abb13720b2c1bae91b6
Wesley Anastasi <anastasicrl@gmail.com>
2025-10-14 18:17:13 -0500

    setting up new start parameters and loading data

---
+++ backend/app/models.py
@@ -0,0 +1,74 @@
+from typing import List
+from sqlalchemy import String, Integer, ForeignKey, BigInteger, CheckConstraint, CHAR, Text
+from sqlalchemy.orm import Mapped, declarative_base, relationship, mapped_column
+
+Base = declarative_base()
+
+
+class Genes(Base):
+    __tablename__ = "Genes"
+
+    name: Mapped[str] = mapped_column(String(50), primary_key=True)
+
+    # Relationships
+    aligned_sequences_fk: Mapped[List["AllignedSequences"]] = relationship(back_populates="gene_fk")
+    regulatory_sequences_fk: Mapped[List["RegulatorySequences"]] = relationship(back_populates="gene_fk")
+
+class Species(Base):
+    __tablename__ = "Species"
+
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+
+    # Relationships
+    aligned_sequences_fk: Mapped[List["AllignedSequences"]] = relationship(back_populates="species_fk")
+    regulatory_sequences_fk: Mapped[List["RegulatorySequences"]] = relationship(back_populates="species_fk")
+
+class RegulatorySequences(Base):
+    __tablename__ = "RegulatorySequences"
+    __table_args__ = (
+        CheckConstraint("start >= 0", name="check_start_nonnegative"),
+        CheckConstraint("end >= start", name="check_end_after_start"),
+    )
+
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    gene = mapped_column(ForeignKey("Genes.name"))
+    species = mapped_column(ForeignKey("Species.name"))
+    start: Mapped[int] = mapped_column(BigInteger)
+    end: Mapped[int] = mapped_column(BigInteger)
+
+    # Relationships
+    gene_fk: Mapped[Genes] = relationship(back_populates="regulatory_sequences_fk")
+    species_fk: Mapped[Species] = relationship(back_populates="regulatory_sequences_fk")
+    regulatoryElements_fk: Mapped[List["RegulatoryElements"]] = relationship(back_populates="regulatorySequences_fk")
+
+class RegulatoryElements(Base):
+    __tablename__ = "RegulatoryElements"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    chromosome: Mapped[int]
+    strand: Mapped[str] = mapped_column(CHAR)
+    geneType: Mapped[str] = mapped_column(String(4))
+    start: Mapped[int]
+    end: Mapped[int]
+    regulatorySequence = mapped_column(ForeignKey("RegulatorySequences.id"))
+
+    __table_args__ = (
+        CheckConstraint("typeStart >= 0", name="check_typeStart_nonnegative"),
+        CheckConstraint("typeEnd >= typeStart", name="check_typeEnd_ge_typeStart"),
+    )
+
+    # Relationships
+    regulatorySequences_fk: Mapped[RegulatorySequences] = relationship(back_populates="regulatoryElements_fk")
+
+class AllignedSequences(Base):
+    __tablename__ = "AllignedSequences"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    sequence: Mapped[str] = mapped_column(Text, )
+    gene = mapped_column(ForeignKey("Genes.name"))
+    species = mapped_column(ForeignKey("Species.name"))
+
+    # Relationships
+    gene_fk: Mapped[Genes] = relationship(back_populates="aligned_sequences_fk")
+    species_fk: Mapped[Species] = relationship(back_populates="aligned_sequences_fk")
