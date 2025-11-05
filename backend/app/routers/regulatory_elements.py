9143518ae8988de70cf739a89945669c0666864c
Wesley Anastasi <anastasicrl@gmail.com>
2025-11-04 20:31:38 -0600

    fixed circual imports

--- backend/app/routers/regulatory_elements.py
+++ backend/app/routers/regulatory_elements.py
@@ -1,10 +1,8 @@
-from typing import Dict, List, Optional
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from pydantic import BaseModel, Field
-from sqlalchemy import or_, select
+from sqlalchemy import select
 from app.models import RegulatorySequences, Species, Genes, RegulatoryElements
-from app.utils import ColorSegment, async_session
-from app.routers import species, regulatory_sequences
+from app.utils import async_session
 from fastapi import APIRouter
 
 class Element(BaseModel):
@@ -41,4 +39,43 @@ async def get_filtered_elements(gene_name: str, species_name: str, element_types
 
     
         return element_list
+    
+
+ALLIGNMENT_GAP = "none"
+NORMAL_GAP = "gray"
+THRESHOLD = 0.009  # Threshold for merging segments
+
+class ColorSegment(BaseModel):
+    color: str = Field(..., description="string representing how it will be displayed on the frontend, if a solid color it will be a hex value")
+    width: float = Field(..., ge=0, le=100, description="Width percentage (0-100)")
+
+# From the parameters generates a list of segments where the widths add up to 100 that can be given to the frontend to display
+async def populate_color_map(total_start: int, total_end: int, sequence_start: int, sequence_end: int, element_list: list[Element]) -> list[ColorSegment]:
+
+    total_width = total_end-total_start
+
+    prev_index = total_start
+
+    color_segment_list: list[ColorSegment] = []
+
+    starting_gap = ((sequence_start - total_start) / total_width) * 100
+
+    if starting_gap != 0:
+        color_segment_list.append(ColorSegment(color=ALLIGNMENT_GAP, width = starting_gap))
+        prev_index = sequence_start
+
+    for element in element_list:
+
+        # if the elements are right not right next to each other we need this to fill in the gap
+        if element.start > prev_index:
+            gap_width = ((element.start - prev_index) / total_width) * 100
+            color_segment_list.append(ColorSegment(color = NORMAL_GAP, width = gap_width))
+            prev_index = element.start
+
+        # using prev_index instead of element.start to handle overlaps
+        element_width = ((element.end - prev_index) / total_width) * 100
+        color_segment_list.append(ColorSegment(color = element.visual, width = element_width))
+        prev_index = element.end
+
+    return color_segment_list
     
--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,8 +1,5 @@
-from pydantic import BaseModel, Field
 from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-
-from app.routers.regulatory_elements import Element
 
 
 
@@ -13,41 +10,3 @@ async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 async_session = async_sessionmaker(async_engine)
 
 metadata = MetaData()
-
-ALLIGNMENT_GAP = "none"
-NORMAL_GAP = "gray"
-THRESHOLD = 0.009  # Threshold for merging segments
-
-class ColorSegment(BaseModel):
-    color: str = Field(..., description="string representing how it will be displayed on the frontend, if a solid color it will be a hex value")
-    width: float = Field(..., ge=0, le=100, description="Width percentage (0-100)")
-
-# From the parameters generates a list of segments where the widths add up to 100 that can be given to the frontend to display
-async def populate_color_map(total_start: int, total_end: int, sequence_start: int, sequence_end: int, element_list: list[Element]) -> list[ColorSegment]:
-
-    total_width = total_end-total_start
-
-    prev_index = total_start
-
-    color_segment_list: list[ColorSegment] = []
-
-    starting_gap = ((sequence_start - total_start) / total_width) * 100
-
-    if starting_gap != 0:
-        color_segment_list.append(ColorSegment(color=ALLIGNMENT_GAP, width = starting_gap))
-        prev_index = sequence_start
-
-    for element in element_list:
-
-        # if the elements are right not right next to each other we need this to fill in the gap
-        if element.start > prev_index:
-            gap_width = ((element.start - prev_index) / total_width) * 100
-            color_segment_list.append(ColorSegment(color = NORMAL_GAP, width = gap_width))
-            prev_index = element.start
-
-        # using prev_index instead of element.start to handle overlaps
-        element_width = ((element.end - prev_index) / total_width) * 100
-        color_segment_list.append(ColorSegment(color = element.visual, width = element_width))
-        prev_index = element.end
-
-    return color_segment_list
